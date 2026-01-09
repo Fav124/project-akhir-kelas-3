9bd4f4c971a4d7a2e0ed5b36eef767e105a4f5d7
Fav124 <favianmuhammadhafiz@gmail.com>
2026-01-09 18:35:16 +0700

    chore: Update environment configuration, adjust database connection, and modify installed package references

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -19,4 +19,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitc61ff82cbf0142a401a48a8161e1595a::getLoader();
+return ComposerAutoloaderInit9417471762e7602a5784e4f284a71d59::getLoader();
